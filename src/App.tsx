fb2d731c98ec1d5eeb04e283a263cf73abc06ac2
Polina <ekortika@gmail.com>
2021-10-09 00:27:43 +0300

    перенаправление по компонентам на главной

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,65 @@
 import React from "react";
-import "./App.css";
-//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import AppBar from "@mui/material/AppBar";
 
 import store from "store/store";
-
+import Moex from "components/Moex";
+import SP500 from "components/SP500";
 import TinkoffTable from "components/Tinkoff";
+import Total from "components/Total";
+
 import "./App.css";
+
+export const routes = {
+  total: {
+    path: "/",
+    exact: true,
+    component: Total,
+  },
+  tinkoff: {
+    path: "/tinkoff",
+    exact: false,
+    component: TinkoffTable,
+  },
+  moex: {
+    path: "/moex",
+    exact: false,
+    component: Moex,
+  },
+  sp500: {
+    path: "/sp500",
+    exact: false,
+    component: SP500,
+  },
+};
 
 function App() {
   return (
     <Provider store={store}>
-      <TinkoffTable />
+      <Router>
+        <AppBar position="static">
+          <Tabs centered>
+            <Tab component="a" label="All exchanges" href="/" />
+            <Tab component="a" label="Tinkoff" href="/tinkoff" />
+            <Tab component="a" label="Moex" href="/moex" />
+            <Tab component="a" label="S&P500" href="/sp500" />
+          </Tabs>
+        </AppBar>
+
+        <Switch>
+          {Object.values(routes).map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          ))}
+        </Switch>
+      </Router>
     </Provider>
   );
 }
