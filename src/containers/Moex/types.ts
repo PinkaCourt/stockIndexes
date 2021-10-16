3e599a2f4afaa3919c327a9a619be8251323a298
Polina <ekortika@gmail.com>
2021-10-16 15:24:51 +0300

    MOEX: типы для мосбиржи

--- src/containers/Moex/types.ts
+++ src/containers/Moex/types.ts
@@ -1,16 +1,88 @@
-import { Position } from "containers/Tinkoff/types";
+import { PositionWeight } from "containers/Tinkoff/types";
 
-export interface Stok {
-  name: string;
+export type DataMOEX = Analytics | Securities;
+export interface Metadata {
+  type: string;
+  bytes?: number;
+  max_size?: number;
+}
+
+export interface MoexResponseData {
+  metadata: Metadata;
+  columns: string[];
+  data: any[];
+}
+export interface Analytics {
+  analytics: MoexResponseData;
+}
+
+export interface Securities {
+  securities: MoexResponseData;
+}
+
+export interface entryMoexIndexStock {
+  indexid: string;
+  secids: string;
+  shortnames: string;
   ticker: string;
-  weight: string;
+  tradedate: string;
+  tradingsession: number;
+  weight: number;
 }
 
-export interface StokMoex extends Stok, Position {
+export interface MoexIndexStockInfo {
+  SECID: string;
+  BOARDID: string;
+  SHORTNAME: string;
+  PREVPRICE: number;
+  LOTSIZE: number;
+  FACEVALUE: number;
+  STATUS: string;
+  BOARDNAME: string;
+  DECIMALS: number;
+  SECNAME: string;
+  REMARKS: string;
+  MARKETCODE: string;
+  INSTRID: string;
+  SECTORID: string;
+  MINSTEP: number;
+  PREVWAPRICE: number;
+  FACEUNIT: string;
+  PREVDATE: Date;
+  ISSUESIZE: number;
+  ISIN: string;
+  LATNAME: string;
+  REGNUMBER: string;
+  PREVLEGALCLOSEPRICE: number;
+  PREVADMITTEDQUOTE: number;
+  CURRENCYID: string;
+  SECTYPE: string;
+  LISTLEVEL: number;
+  SETTLEDATE: Date;
+}
+
+export interface MoexStockMap {
+  [key: string]: entryMoexIndexStock;
+}
+
+export interface MoexSecuritiesMap {
+  [key: string]: MoexIndexStockInfo;
+}
+
+export interface StocksMRBCFull extends entryMoexIndexStock {
+  isin: string;
+  issueSize: number;
+  prevPrice: number;
+  stockCapitalization: number;
   weightInPortfolio?: number;
-  bye?: number;
+  balance?: string;
+  toBuy?: number;
 }
 
-export interface StokMap {
-  [key: string]: StokMoex;
+export interface StocksMRBCFullMap {
+  [key: string]: StocksMRBCFull;
+}
+
+export interface ExpectedStocksWeight {
+  [key: string]: StocksMRBCFull;
 }
