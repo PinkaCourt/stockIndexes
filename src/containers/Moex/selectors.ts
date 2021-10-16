48abc2769c6c3f7439fa7f526bd76c8d840aa5cf
Polina <ekortika@gmail.com>
2021-10-16 15:32:39 +0300

    MOEX: селекторы для мосбиржи

--- src/containers/Moex/selectors.ts
+++ src/containers/Moex/selectors.ts
@@ -1,7 +1,43 @@
-import { RootState } from "store/store";
+import { createSelector } from "reselect";
 
-export const selectMOEXstoks = (state: RootState) => state.moex.stocks;
+import { getSecurityCapitalization } from "common/utils";
+import { RootState } from "store/store";
+import { StocksMRBCFullMap } from "./types";
+
+export const selectStocksMRBC = (state: RootState) => state.moex.stocksMRBC;
+export const selectAllStocksInfo = (state: RootState) =>
+  state.moex.allStocksInfo;
+export const selectExpectedStocksWeight = (state: RootState) =>
+  state.moex.expectedStocksWeight;
+
+export const selectStocksMRBCFull = createSelector(
+  selectStocksMRBC,
+  selectAllStocksInfo,
+  (stocks, stockInfo) => {
+    if (!stocks || !stockInfo) {
+      return;
+    }
+
+    const StocksMRBCFullMap = Object.values(stocks).reduce((accum, current) => {
+      accum[current.ticker] = {
+        ...current,
+        isin: stockInfo[current.ticker]?.ISIN || "",
+        issueSize: stockInfo[current.ticker]?.ISSUESIZE || 0,
+        prevPrice: stockInfo[current.ticker]?.PREVPRICE || 0,
+        stockCapitalization: getSecurityCapitalization(
+          stockInfo[current.ticker]?.PREVPRICE,
+          stockInfo[current.ticker]?.ISSUESIZE
+        ),
+      };
+      return accum;
+    }, {} as StocksMRBCFullMap);
+
+    return StocksMRBCFullMap;
+  }
+);
 
 export const selectMoexState = {
-  stocks: selectMOEXstoks,
+  stocksMRBC: selectStocksMRBC,
+  allStocksInfo: selectAllStocksInfo,
+  stocksMRBCFull: selectStocksMRBCFull,
 };
