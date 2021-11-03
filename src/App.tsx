5bdff11679478312d3856944eeaf0bea5404316f
Polina <ekortika@gmail.com>
2021-11-04 00:55:15 +0300

    FormPage: добавляем в приложение диалог и новый аппбар

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import AppBar from "@mui/material/AppBar";
 
 import store from "store/store";
 import Moex from "components/Moex";
 import SP500 from "components/SP500";
 import TinkoffTable from "components/Tinkoff";
-import Total from "components/Total";
+import UserDataDialog from "components/UserDataDialog";
+
+import AppBarMui from "components/AppBarMui";
 
 import "./App.css";
 
 export const routes = {
-  total: {
-    path: "/",
-    exact: true,
-    component: Total,
-  },
   tinkoff: {
     path: "/tinkoff",
     exact: false,
+    label: "Tinkoff",
     component: TinkoffTable,
   },
   moex: {
     path: "/moex",
     exact: false,
+    label: "Moex",
     component: Moex,
   },
   sp500: {
     path: "/sp500",
     exact: false,
+    label: "S&P500",
     component: SP500,
   },
 };
@@ -39,16 +36,11 @@ export const routes = {
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <AppBar position="static">
-          <Tabs centered>
-            <Tab component="a" label="All exchanges" href="/" />
-            <Tab component="a" label="Tinkoff" href="/tinkoff" />
-            <Tab component="a" label="Moex" href="/moex" />
-            <Tab component="a" label="S&P500" href="/sp500" />
-          </Tabs>
-        </AppBar>
-
+      <BrowserRouter>
+        <UserDataDialog />
+        <Route path="/">
+          <AppBarMui tabs={routes} />
+        </Route>
         <Switch>
           {Object.values(routes).map((route) => (
             <Route
@@ -59,7 +51,7 @@ function App() {
             />
           ))}
         </Switch>
-      </Router>
+      </BrowserRouter>
     </Provider>
   );
 }
