5b38e04504aba6e90d9f3bcf7873b84a6266d017
Polina <ekortika@gmail.com>
2021-11-04 00:34:46 +0300

    FormPage: В глобальный редьюсер добавляем введенные данные от пользователя

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
+import userDataReducer from "containers/UserData/reducer";
 import tinkoffReducer from "containers/Tinkoff/reducer";
 import moexReducer from "containers/Moex/reducer";
 
 const rootReducer = combineReducers({
+  userData: userDataReducer,
   tinkoff: tinkoffReducer,
   moex: moexReducer,
 });
