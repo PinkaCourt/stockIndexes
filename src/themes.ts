9ab453b41d7fad19ff3319f661958b93ded4b62c
Polina <ekortika@gmail.com>
2021-11-06 01:41:15 +0300

    FormPage: темы

---
+++ src/themes.ts
@@ -0,0 +1,69 @@
+import { createTheme } from "@mui/material/styles";
+
+export const themeDF = createTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
+
+export const themeTF = createTheme({
+  palette: {
+    primary: {
+      light: "#ffff66",
+      main: "#ffdd2d",
+      dark: "#fcc521",
+      contrastText: "#000000",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#000000",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
+
+export const themeMOEX = createTheme({
+  palette: {
+    primary: {
+      light: "#fd575c",
+      main: "#c41a32",
+      dark: "#8c000c",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      light: "#6e8da2",
+      main: "#416073",
+      dark: "#143648",
+      contrastText: "#ffffff",
+    },
+  },
+});
+
+export const themeSP500 = createTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
