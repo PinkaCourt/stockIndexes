230769e88973f461ce4c37b286dc0dff2b953afe
Polina <ekortika@gmail.com>
2021-11-07 20:42:42 +0300

    Sorting: tinkoff таблица с сортировкой

--- src/components/Tinkoff/index.tsx
+++ src/components/Tinkoff/index.tsx
@@ -1,40 +1,81 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Table from "@mui/material/Table";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import TableBody from "@mui/material/TableBody";
+import TableSortLabel from "@mui/material/TableSortLabel";
 
-import { selectTFPortfolio } from "containers/Tinkoff/selectors";
+import { Direction } from "common/types";
+import { compareFn, revertDirection } from "common/utils";
+import {
+  selectTFPortfolio,
+  selectDirection,
+  selectOrderBy,
+} from "containers/Tinkoff/selectors";
+import { setDirectionTF, setOrderByTF } from "containers/Tinkoff/actions";
+import { OrderByTF, Position } from "containers/Tinkoff/types";
 
 const TinkoffTable = () => {
   const securities = useSelector(selectTFPortfolio);
+  const direction = useSelector(selectDirection);
+  const orderBy = useSelector(selectOrderBy);
+
+  const dispatch = useDispatch();
+
+  const sortedSecurities = React.useMemo(() => {
+    if (!securities) {
+      return [];
+    }
+    return compareFn(
+      Object.values(securities),
+      orderBy,
+      direction
+    ) as Position[];
+  }, [direction, orderBy, securities]);
 
   if (!securities) {
     return null;
   }
 
   const tableHeads = [
-    "Название ценной бумаги",
-    "Тикер",
-    "Количество, шт",
-    "Количество лотов",
-    "Oжидаемая доходность",
-    "Средняя цена позиции",
-  ];
+    { id: "name", name: "Название ценной бумаги" },
+    { id: "ticker", name: "Тикер" },
+    { id: "balance", name: "Количество, шт" },
+    { id: "lots", name: "Количество лотов" },
+    { id: "expectedYield", name: "Oжидаемая доходность" },
+    { id: "averagePositionPrice", name: "Средняя цена позиции" },
+  ] as {
+    id: OrderByTF;
+    name: string;
+  }[];
+
+  const sortHandler = (order: OrderByTF) => {
+    if (orderBy === order) {
+      dispatch(setDirectionTF(revertDirection[direction] as Direction));
+    } else {
+      dispatch(setOrderByTF(order));
+    }
+  };
 
   return (
     <Table size="small" stickyHeader>
       <TableHead>
         <TableRow>
-          {tableHeads.map((tableHead, idx) => {
-            return <TableCell key={idx}>{tableHead}</TableCell>;
+          {tableHeads.map(({ id, name }) => {
+            return (
+              <TableCell key={id} onClick={() => sortHandler(id)}>
+                <TableSortLabel active={orderBy === id} direction={direction}>
+                  {name}
+                </TableSortLabel>
+              </TableCell>
+            );
           })}
         </TableRow>
       </TableHead>
       <TableBody>
-        {Object.values(securities).map(
+        {sortedSecurities.map(
           ({
             name,
             ticker,
