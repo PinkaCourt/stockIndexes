97aeab98468d21c03b2552741c03946ca1900dc9
Polina <ekortika@gmail.com>
2021-11-08 20:36:39 +0300

    Sorting в константы добавляем индексы примерной стоимости валют

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,6 +2,12 @@ export const RUB = "RUB";
 export const EUR = "EUR";
 export const USD = "USD";
 
+export const currencyMap = {
+  RUB: 1,
+  USD: 70,
+  EUR: 80,
+};
+
 export const STOCK = "Stock";
 export const BOND = "Bond";
 export const ETF = "Etf";
