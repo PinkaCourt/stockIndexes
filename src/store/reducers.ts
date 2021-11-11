97957c52014598f8419a8cd30fe3fa54a26a44ec
Polina <ekortika@gmail.com>
2021-11-12 00:44:53 +0300

    Sorting: добавляем курсы валют к стору

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 
 import userDataReducer from "containers/UserData/reducer";
+import exchangeRatesReducer from "containers/exchangeRates/reducer";
 import tinkoffReducer from "containers/Tinkoff/reducer";
 import moexReducer from "containers/Moex/reducer";
 
 const rootReducer = combineReducers({
   userData: userDataReducer,
+  exchangeRates: exchangeRatesReducer,
   tinkoff: tinkoffReducer,
   moex: moexReducer,
 });
