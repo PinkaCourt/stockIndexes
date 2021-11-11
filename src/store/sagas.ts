2b14ca036b2da4c80b2d468e13850854d386cdbe
Polina <ekortika@gmail.com>
2021-11-12 00:45:18 +0300

    Sorting: запускаем сагу получения курсов валют сразу

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -2,8 +2,10 @@ import { fork } from "redux-saga/effects";
 
 import tinkoffSaga from "containers/Tinkoff/sagas";
 import moexWatcher from "containers/Moex/sagas";
+import exchangeRatesWatcher from "containers/exchangeRates/saga";
 
 export default function* initSaga() {
+  yield fork(exchangeRatesWatcher);
   yield fork(tinkoffSaga);
   yield fork(moexWatcher);
 }
