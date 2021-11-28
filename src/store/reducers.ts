541d06cdcafa95ff7e7e5ff83abfaf040799bdfc
Polina <ekortika@gmail.com>
2021-11-28 17:56:29 +0300

    SP500: редьюсер сп500 в общий список

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -4,12 +4,14 @@ import userDataReducer from "containers/UserData/reducer";
 import exchangeRatesReducer from "containers/exchangeRates/reducer";
 import tinkoffReducer from "containers/Tinkoff/reducer";
 import moexReducer from "containers/Moex/reducer";
+import sp500Reducer from "containers/SP500/reducer";
 
 const rootReducer = combineReducers({
   userData: userDataReducer,
   exchangeRates: exchangeRatesReducer,
   tinkoff: tinkoffReducer,
   moex: moexReducer,
+  sp500: sp500Reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
