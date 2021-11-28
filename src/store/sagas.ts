3b6c8b33c36ee8f48ccbf64827d9e040dafc12b0
Polina <ekortika@gmail.com>
2021-11-28 17:56:47 +0300

    SP500: сага сп500 в общий список

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -3,9 +3,11 @@ import { fork } from "redux-saga/effects";
 import tinkoffSaga from "containers/Tinkoff/sagas";
 import moexWatcher from "containers/Moex/sagas";
 import exchangeRatesWatcher from "containers/exchangeRates/saga";
+import sp500Watcher from "containers/SP500/saga";
 
 export default function* initSaga() {
   yield fork(exchangeRatesWatcher);
   yield fork(tinkoffSaga);
   yield fork(moexWatcher);
+  yield fork(sp500Watcher);
 }
