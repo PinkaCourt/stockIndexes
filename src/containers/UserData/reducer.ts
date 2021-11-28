2f28657c0d4e60a03ebfc8ee2b810a95024fa66a
Polina <ekortika@gmail.com>
2021-11-28 14:52:00 +0300

    SP500: рефакторинг юзердаты

--- src/containers/UserData/reducer.ts
+++ src/containers/UserData/reducer.ts
@@ -1,22 +1,42 @@
-import { createReducer } from "@reduxjs/toolkit";
-
-import { setTinkoffToken, setWishedRuPortfolio } from "./actions";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface InitState {
   tinkoffToken: string;
   ruPortfolio: number;
+  usdPortfolio: number;
+  eurPortfolio: number;
 }
 
-const initState: InitState = {
+const initialState: InitState = {
   tinkoffToken: "",
   ruPortfolio: 0,
+  usdPortfolio: 0,
+  eurPortfolio: 0,
 };
 
-export default createReducer(initState, (builder) => {
-  builder.addCase(setTinkoffToken, (state, { payload }) => {
-    state.tinkoffToken = payload;
-  });
-  builder.addCase(setWishedRuPortfolio, (state, { payload }) => {
-    state.ruPortfolio = payload;
-  });
+const userDataSlice = createSlice({
+  name: "userData",
+  initialState,
+  reducers: {
+    setTinkoffToken(state, { payload }: PayloadAction<string>) {
+      return { ...state, tinkoffToken: payload };
+    },
+    setWishedRuPortfolio(state, { payload }: PayloadAction<number>) {
+      return { ...state, ruPortfolio: payload };
+    },
+    setWishedUsdPortfolio(state, { payload }: PayloadAction<number>) {
+      return { ...state, usdPortfolio: payload };
+    },
+    setWishedEurPortfolio(state, { payload }: PayloadAction<number>) {
+      return { ...state, eurPortfolio: payload };
+    },
+  },
 });
+
+export const {
+  setTinkoffToken,
+  setWishedRuPortfolio,
+  setWishedUsdPortfolio,
+  setWishedEurPortfolio,
+} = userDataSlice.actions;
+export default userDataSlice.reducer;
